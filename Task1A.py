d7a13c95e8f7dada2634691907d57e7e6ea15d18
skv221 <www.venkatesan.skv@gmail.com>
2024-12-16 18:06:31 +0530

    Added comments

--- Task1A.py
+++ Task1A.py
@@ -1,11 +1,11 @@
 import csv
-from prettytable import PrettyTable
+from prettytable import PrettyTable #For better visualisation
 
 #with open('D:\Selenium Practices\Week 4\Sample.csv', mode='r') as file: #Check with this line for large set of data
-with open('D:\Selenium Practices\Week 4\Sample2.csv', mode='r') as file:
+with open('D:\Selenium Practices\Week 4\Sample2.csv', mode='r') as file: 
     csvFile = csv.reader(file)
-    title = next(csvFile)
-    table = PrettyTable(title)
+    title = next(csvFile) #taking the first line to print it as header
+    table = PrettyTable(title) #Table initialisation
     for value in csvFile:
         table.add_row(value)
     print(table)
