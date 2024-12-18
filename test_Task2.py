8d1efa94ac3d6648327844c26785b5d1ba2e0c3d
skv221 <www.venkatesan.skv@gmail.com>
2024-12-18 18:49:31 +0530

    Comments were added

--- test_Task2.py
+++ test_Task2.py
@@ -3,16 +3,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+#Sending multiple test cases and expectations as Parameters
 @pytest.mark.parametrize("name, email, message", [
     ("Aditi Sharma","aditi.sharma@gmail.com",True),
     ("Rajesh Gupta","rajesh.gupta@",False),
     ("Vikram Malhotra","vikram.malhotra@gmail.com",True),
     ("","priya.verma@gmail.com",False)
 ])
+
+#Function for sending name and mail to the form
 def test_submit(setup_browser, name, email, message):
-    driver = setup_browser
+    driver = setup_browser #this one can be retrieved from pytest fixture
     driver.get("https://qavalidation.com/demo-form/")
     
+    #Sending the parameters to the form
     driver.find_element(By.NAME, "g4072-fullname").send_keys(name)
     driver.find_element(By.NAME, "g4072-email").send_keys(email)
     driver.find_element(By.XPATH, "//button[@type='submit']").click()
